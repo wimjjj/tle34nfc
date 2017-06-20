b90196ffae25f1942ee720657af88326a7863a67
Siskofasa <school@assetessa.nl>
2017-06-20 12:09:51 +0200

    made explanation more responsive

--- app/src/main/java/com/scanner/tle34/wimjo/tle34nfc/Explanation.java
+++ app/src/main/java/com/scanner/tle34/wimjo/tle34nfc/Explanation.java
@@ -1,9 +1,11 @@
 package com.scanner.tle34.wimjo.tle34nfc;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Explanation extends AppCompatActivity {
 
@@ -12,6 +14,14 @@ public class Explanation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_explanation);
+    }
+
+    private void setFonts() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/WalterTurncoat.ttf");
+
+        ((TextView) findViewById(R.id.title)).setTypeface(typeface);
+        ((TextView) findViewById(R.id.exp_text)).setTypeface(typeface);
+        ((TextView) findViewById(R.id.explanation_button)).setTypeface(typeface);
     }
 
     public void toMenu (View v) {
