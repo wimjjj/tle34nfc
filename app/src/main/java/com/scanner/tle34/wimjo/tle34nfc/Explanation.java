35921c34178404f1e8196ef44411ba5df248a1d9
Siskofasa <school@assetessa.nl>
2017-06-20 11:59:06 +0200

    exp finished

--- app/src/main/java/com/scanner/tle34/wimjo/tle34nfc/Explanation.java
+++ app/src/main/java/com/scanner/tle34/wimjo/tle34nfc/Explanation.java
@@ -1,7 +1,9 @@
 package com.scanner.tle34.wimjo.tle34nfc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Explanation extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Explanation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_explanation);
+    }
+
+    public void toMenu (View v) {
+       finish();
     }
 }
