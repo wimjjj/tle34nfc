2faf196c9d5c16211fde21b46daf8870b414ad92
wimjjj <wim.jongeneel1@gmail.com>
2017-06-20 11:36:30 +0200

    added score to endlevelscreen

--- app/src/main/java/com/scanner/tle34/wimjo/tle34nfc/LevelCompleteActivity.java
+++ app/src/main/java/com/scanner/tle34/wimjo/tle34nfc/LevelCompleteActivity.java
@@ -1,10 +1,13 @@
 package com.scanner.tle34.wimjo.tle34nfc;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.media.MediaPlayer;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class LevelCompleteActivity extends AppCompatActivity {
 
@@ -16,6 +19,15 @@ public class LevelCompleteActivity extends AppCompatActivity {
 
         playSound(R.raw.level_complete);
 
+        setFonts();
+
+        Intent intent = getIntent();
+        showScore(intent.getIntExtra("SCORE", 1));
+
+    }
+
+    private void showScore(int score){
+        ((TextView) findViewById(R.id.score)).setText(String.valueOf(score));
     }
 
     private void playSound(int id){
@@ -30,5 +42,12 @@ public class LevelCompleteActivity extends AppCompatActivity {
         Intent highscore = new Intent(getBaseContext(), Highscore.class);
         startActivity(highscore);
         finish();
+    }
+
+    private void setFonts() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/WalterTurncoat.ttf");
+
+        ((TextView) findViewById(R.id.score)).setTypeface(typeface);
+        ((TextView) findViewById(R.id.msg)).setTypeface(typeface);
     }
 }
